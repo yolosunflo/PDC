20b663faecccf823e992c93170870331d001c78f
alexdorsaz <160587479+alexdorsaz@users.noreply.github.com>
2026-05-29 11:46:12 +0200

    clean du main

--- main.py
+++ main.py
@@ -50,27 +50,6 @@ def main():
     print(f"Sent    : '{message}'")
     print(f"Decoded : '{decoded}'")
     print(f"Errors  : {errors}/{len(message)}")
-# def main():
-
-#     message = sys.argv[1]
-
-#     # ENCODE
-#     x = encode_message(message)
-
-#     print("Sent signal:")
-#     print(x)
-
-#     # CHANNEL
-#     y = channel(x)
-
-#     print("Received signal:")
-#     print(y)
-
-#     # DECODE
-#     decoded = decode_message(y)
-
-#     print("Decoded message:")
-#     print(decoded)
 
 if __name__ == "__main__":
     main()
